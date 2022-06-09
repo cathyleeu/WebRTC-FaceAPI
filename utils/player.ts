25547906652ea19aa222a82593431d79159a6a3e
Cathy Lee <leeyu823@gmail.com>
2022-06-09 20:47:04 +0900

    add : utils

--- utils/player.ts
+++ utils/player.ts
@@ -27,3 +27,26 @@ export function getConstraints({audioId, cameraId}: {audioId?: string, cameraId?
     }
   })
 };
+
+// taken from https://jameshfisher.com/2021/01/18/measuring-audio-volume-in-javascript/
+export function getSoundMeter(stream: MediaStream) {
+  if(window.AudioContext) {
+    const audioContext = new AudioContext();
+    const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+    const analyserNode = audioContext.createAnalyser();
+    mediaStreamAudioSourceNode.connect(analyserNode);
+
+    const pcmData = new Float32Array(analyserNode.fftSize);
+    let meterValue;
+    const onFrame = () => {
+        analyserNode.getFloatTimeDomainData(pcmData);
+        let sumSquares = 0.0;
+        for (const amplitude of pcmData) { sumSquares += amplitude*amplitude; }
+
+        // volume Meter value 
+        meterValue = Math.sqrt(sumSquares / pcmData.length);
+        window.requestAnimationFrame(onFrame);
+    };
+    return meterValue
+  }
+}
